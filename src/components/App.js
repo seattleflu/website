1810f9677ecc81e8f5a7bd377c2ab21b1c64c6a0
Trevor Bedford <trevor@bedford.io>
2019-01-06 15:51:20 -0800

    Scroll to top on route change
    Without this, clicking the link to /faq would start half way down the page. I found this solution via https://reacttraining.com/react-router/web/guides/scroll-restoration

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { ThemeProvider } from 'styled-components'
+import ScrollToTop from "./ScrollToTop";
 import { GlobalStyle, theme } from "../theme/globalStyle";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
@@ -13,19 +14,20 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <ThemeProvider theme={theme}>
-          <>
-            <GlobalStyle/>
-            <Header/>
-            <Route exact path="/" component={Splash}/>
-            <Route path="/faq" component={FAQ}/>
-            <Route path="/flu101" component={Flu101}/>
-            <Route path="/kiosks" component={Kiosks}/>
-            <Footer/>
-          </>
-        </ThemeProvider>
+        <ScrollToTop>
+          <ThemeProvider theme={theme}>
+            <>
+              <GlobalStyle/>
+              <Header/>
+              <Route exact path="/" component={Splash}/>
+              <Route path="/faq" component={FAQ}/>
+              <Route path="/flu101" component={Flu101}/>
+              <Route path="/kiosks" component={Kiosks}/>
+              <Footer/>
+            </>
+          </ThemeProvider>
+        </ScrollToTop>
       </Router>
-
     );
   }
 }
