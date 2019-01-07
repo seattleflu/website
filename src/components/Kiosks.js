d168e445c1466163703fcc46a2f52863ae5d24dd
james hadfield <jh22@sanger.ac.uk>
2019-01-07 16:51:56 +1300

    adjust kiosk image for mobile

--- src/components/Kiosks.js
+++ src/components/Kiosks.js
@@ -9,9 +9,13 @@ const Flex = styled.div`
   justify-content: space-between;
   margin: 0 auto 3em;
   max-width: 1080px;
+  @media (max-width: 1080px) {
+    max-width: 80vw;
+  }
   @media (max-width: 735px) {
     flex-direction: column;
-    margin: 0 3em 3em;
+    max-width: 100vw;
+    margin: 0 0 3em;
   }
 `
 
@@ -20,7 +24,7 @@ export const Kiosks = () => (
     <utils.ContentContainer>
       <utils.H1>Kiosk locations</utils.H1>
       <Flex>
-        <img src={kiosks} alt="kiosks"/>
+        <img src={kiosks} alt="kiosks" width="100%"/>
       </Flex>
     </utils.ContentContainer>
   </utils.OuterContainer>
