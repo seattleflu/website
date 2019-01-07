950b65db90be4ab383d2ede353f9587588000ea6
james hadfield <jh22@sanger.ac.uk>
2019-01-07 23:34:55 +1300

    remove Flu101 page
    Content is now part of the splash page

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,6 @@ import Header from "./Header";
 import { Footer } from "./Footer";
 import { Splash } from "./Splash";
 import { FAQ } from "./FAQ";
-import { Flu101 } from "./Flu101";
 import { Kiosks } from "./Kiosks";
 
 class App extends Component {
@@ -22,7 +21,6 @@ class App extends Component {
                 <Header/>
                 <Route exact path="/" component={Splash}/>
                 <Route path="/faq" component={FAQ}/>
-                <Route path="/flu101" component={Flu101}/>
                 <Route path="/kiosks" component={Kiosks}/>
                 <Footer/>
               </PageContainer>
