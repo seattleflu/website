8ca880c7afc7f9ceb79b904df9b56d64ce5e6897
Trevor Bedford <trevor@bedford.io>
2019-01-06 19:13:31 -0800

    Update favicon

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('port', process.env.PORT || 5000);
 app.use(sslRedirect()); // redirect HTTP to HTTPS
 app.use(require('express-naked-redirect')({reverse: true})); // redirect www.seattleflu.org to seattleflu.org
 app.use(express.static(path.join(__dirname, 'build')));
-app.use('/favicon.ico', express.static(path.join(__dirname, "build", "favicon.ico")));
+app.use('/favicon.png', express.static(path.join(__dirname, "build", "favicon.png")));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
