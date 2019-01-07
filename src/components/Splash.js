476ba6e2878a043a1ed41fb2ab5d737be5ee2e2d
james hadfield <jh22@sanger.ac.uk>
2019-01-07 16:29:24 +1300

    Feature block with optional right-hand-sidebar
    <Feature> has an optional button (if props.buttonText and props.buttonLink are used) and will display a right-hand sidebar of text if a second child is provided.

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -5,6 +5,7 @@ import logo from '../img/logo.png';
 export const Splash = () => (
   <utils.OuterContainer>
     <utils.ContentContainer>
+
       <utils.HeroContainer>
         <utils.HeroBlock
           title="Help us outsmart the flu"
@@ -35,9 +36,12 @@ export const Splash = () => (
         </>
         </utils.HeroBlock>
       </utils.HeroContainer>
-      <utils.FeatureContainer>
-        <utils.FeatureBlock
-          title="Flu 101"
+
+      
+      <utils.Feature
+          title="You can help us"
+          buttonLink="/kiosks"
+          buttonText="Find a Kiosk"
         >
           {`
             Today, up to 80,000 people in the United States die from the flu each year, and more than double that number get sick and suffer.
@@ -49,37 +53,46 @@ export const Splash = () => (
             This is important because anti-viral medications, like Tamiflu are only effective against the flu virus.
             (pull these two sentences right)
           `}
-        </utils.FeatureBlock>
-      </utils.FeatureContainer>
-      <utils.FeatureContainer>
-        <utils.FeatureBlock
+        </utils.Feature>
+
+        <utils.Feature
           title="You can help us"
-          href="/kiosks"
-          buttonTitle="Find a Kiosk"
+          buttonLink="/kiosks"
+          buttonText="Find a Kiosk"
         >
-          {`
-            We've set up a number of kiosks around Seattle to gather samples from people who feel like they may have the flu.
-            If you are feeling ill and see a kiosk, you can take 15 minutes to participate (we’ll work extra hard to distract you from feeling crummy).
-            a. We’ll make sure that you have 2 or more flu-like symptoms (fever, cough, sore throat, runny or stuffy nose, muscle or body aches).
-            b. You’ll answer some questions about your health and where you live (geography matters because we are mapping where the flu is).
-            c. We’ll take a swab from the front of your nose to collect the exact virus you have.
-            d. You’re done! Enjoy your $10 Starbucks gift card for participating!
-            (make this a proper list)
-          `}
-        </utils.FeatureBlock>
-      </utils.FeatureContainer>
-      <utils.FeatureContainer>
-        <utils.FeatureBlock
+          <div>
+            <span>
+              {`
+                We've set up a number of kiosks around Seattle to gather samples from people 
+                who feel like they may have the flu. If you are feeling ill and see a kiosk, 
+                you can take 15 minutes to participate (we’ll work extra hard to distract you 
+                from feeling crummy).
+              `}
+            </span>
+            <ol type="a">
+              <li>We’ll make sure that you have 2 or more flu-like symptoms (fever, cough, sore throat, runny or stuffy nose, muscle or body aches).</li>
+              <li>You’ll answer some questions about your health and where you live (geography matters because we are mapping where the flu is).</li>
+              <li>We’ll take a swab from the front of your nose to collect the exact virus you have.</li>
+              <li>You’re done! Enjoy your $10 Starbucks gift card for participating!</li>
+            </ol>
+          </div>
+          <div>
+            SOME SIDE TEXT SOME SIDE TEXT SOME SIDE TEXT SOME SIDE TEXT SOME SIDE TEXT
+          </div>
+        </utils.Feature>
+
+        <utils.Feature
           title="Smarter. Faster. Wellness."
-          href="/faq"
-          buttonTitle="Questions?"
+          buttonLink="/faq"
+          buttonText="Questions?"
         >
           {`
             We’re on a mission to understand how the flu enters and spreads through a community because the more we know the faster and smarter we can be.
             Have any other questions? Check out our FAQ.
           `}
-        </utils.FeatureBlock>
-      </utils.FeatureContainer>
+        </utils.Feature>
+
+
     </utils.ContentContainer>
   </utils.OuterContainer>
 );
--- src/components/utils.js
+++ src/components/utils.js
@@ -8,6 +8,11 @@ export const OuterContainer = styled.div`
   @media (max-width: 735px) {
     max-width: 90vw;
   }
+  @media (max-width: 500px) {
+    width: 100%;
+    max-width: 100%;
+    margin: 2em 0;
+  }
 `
 export const ContentContainer = styled.div`
   padding: 0px;
@@ -74,65 +79,7 @@ export const HeroBlock = (props) => {
   )
 }
 
-export const FeatureContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  color: ${props => props.theme.neutral100};
-  background-color: ${props => props.theme.primary600};
-  padding: 4em 3em;
-  margin: 2em auto;
-  @media (max-width: 735px) {
-    flex-direction: column;
-    padding: 2em 0em;
-  }
-`
-export const FeatureBlock = (props) => {
-  const BlockContainer = styled.div`
-    margin: 0 2em;
-    flex: 1 0 20%;
-  `
-  const LinkContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-  `
-  const LinkButton = styled(Link)`
-    border: 1px solid ${props => props.theme.neutral200};
-    background-color: inherit;
-    color: ${props => props.theme.neutral200};
-    border-radius: 3px;
-    padding: 10px;
-    text-transform: uppercase;
-    transition: background .3s, color .3s;
-    font-weight: 800;
-    font-size: 14px;
-    cursor: pointer;
-    text-decoration: none;
-    &:hover {
-      background-color: ${props => props.theme.accent500};
-      color: ${props => props.theme.neutral100};
-    }
-  `
-  const Title = styled(H2)`
-    color: ${props => props.theme.neutral200};
-  `
-  return (
-    <BlockContainer>
-      <Title>{props.title}</Title>
-      <CenteredParagraph>{props.children}</CenteredParagraph>
-      {
-        props.buttonTitle ?
-        <LinkContainer>
-          <LinkButton to={props.href}>
-            {props.buttonTitle}
-          </LinkButton>
-        </LinkContainer>
-        : ""
-      }
-    </BlockContainer>
-  )
-}
+
 const BaseHeader = styled.div`
   font-weight: 600;
   text-align: ${props => props.left ? "left" : "center"};
@@ -153,6 +100,100 @@ export const H3 = styled(BaseHeader)`
 export const Bold = styled.span`
   font-weight: 600;
 `
+
+/* Feature Block */
+const FeatureContainer = styled.div`
+  color: ${props => props.theme.neutral100};
+  background-color: ${props => props.theme.primary600};
+  padding: 3em 3em;
+  margin: 4em auto;
+  @media (max-width: 735px) {
+    flex-direction: column;
+    padding: 2em 2em;
+  }
+`
+const FeatureButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`
+const FeatureButton = styled(Link)`
+  border: 1px solid ${props => props.theme.neutral200};
+  background-color: inherit;
+  color: ${props => props.theme.neutral200};
+  border-radius: 3px;
+  padding: 10px;
+  text-transform: uppercase;
+  transition: background .3s, color .3s;
+  font-weight: 800;
+  font-size: 14px;
+  cursor: pointer;
+  text-decoration: none;
+  &:hover {
+    background-color: ${props => props.theme.accent500};
+    color: ${props => props.theme.neutral100};
+  }
+`
+const FeatureTitle = styled(H2)`
+  color: ${props => props.theme.neutral200};
+  text-align: center;
+`
+const FeatureFlexContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-top: 2em;
+  margin-bottom: 2em;
+  line-height: 1.7rem;
+  @media (max-width: 735px) {
+    flex-direction: column;
+    line-height: 1.4rem;
+  }
+`
+const FeatureMainText = styled.div`
+  flex: 7 7 70%;
+`;
+const FeatureSpacer = styled.div`
+  flex: 1 1 10%;
+`
+const FeatureSidebar = styled.div`
+  flex: 2 2 20%;
+  text-align: center;
+  font-size: 0.9rem;
+  @media (max-width: 735px) {
+    margin-top: 2em;
+  }
+`;
+
+export const Feature = (props) => {
+  const children = React.Children.toArray(props.children);
+  return (
+    <FeatureContainer>
+      <FeatureTitle>{props.title}</FeatureTitle>
+      <FeatureFlexContainer>
+        <FeatureMainText>
+          {children[0]}
+        </FeatureMainText>
+        {children[1] ? (
+          <>
+            <FeatureSpacer/>
+            <FeatureSidebar>
+              {children[1]}
+            </FeatureSidebar>
+          </>
+        ) : null}
+      </FeatureFlexContainer>
+      {(props.buttonText && props.buttonLink) ? (
+        <FeatureButtonContainer>
+          <FeatureButton to={props.buttonLink}>
+            {props.buttonText}
+          </FeatureButton>
+        </FeatureButtonContainer>
+      ) : null}
+    </FeatureContainer>
+  );
+}
+
 
 const baseLink = css`
   cursor: pointer;
