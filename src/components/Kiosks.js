6836d7952ad6a021f11ae38296a3fe14eb5ad0ac
james hadfield <jh22@sanger.ac.uk>
2019-01-08 11:08:52 +1300

    remove exact kiosk locations

--- src/components/Kiosks.js
+++ src/components/Kiosks.js
@@ -5,7 +5,7 @@ import kiosks from '../img/seattle-map.jpg';
 
 const Flex = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   margin: 2em auto 3em;
   max-width: 1080px;
   @media (max-width: 1080px) {
@@ -18,16 +18,16 @@ const Flex = styled.div`
   }
 `
 
-const OL = styled.ol`
-  line-height: 5vw;
-  margin: 0 2em;
-  color: ${props => props.theme.primary600}
-  font-weight: 600;
-  @media (max-width: 500px) {
-    line-height: 22px;
-    margin: 2em auto 0;
-  }
-`
+// const OL = styled.ol`
+//   line-height: 5vw;
+//   margin: 0 2em;
+//   color: ${props => props.theme.primary600}
+//   font-weight: 600;
+//   @media (max-width: 500px) {
+//     line-height: 22px;
+//     margin: 2em auto 0;
+//   }
+// `
 const ImgContainer = styled.div`
   max-width: 40vw;
   @media (max-width: 500px) {
@@ -50,18 +50,6 @@ export const Kiosks = () => (
         <ImgContainer>
           <Img src={kiosks} alt="kiosks"/>
         </ImgContainer>
-        <OL>
-          <li>Northwest Hospital</li>
-          <li>Children's Hospital</li>
-          <li>University of Washington</li>
-          <li>UW Daycare</li>
-          <li>Fred Hutch</li>
-          <li>Fred Hutch Daycare</li>
-          <li>Harborview</li>
-          <li>Homeless Shelters (St. Martin Shelter, DESC)</li>
-          <li>Sea Mar Clinic</li>
-          <li>SEATAC (International)</li>
-        </OL>
       </Flex>
     </utils.ContentContainer>
   </utils.OuterContainer>
