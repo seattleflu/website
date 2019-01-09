4b8e821afb6a8e368bdf9c1fc8d90b85a3765a8c
james hadfield <jh22@sanger.ac.uk>
2019-01-09 16:15:04 +1300

    add compression to server responses
    non-binary files will be served using gzip compression (if the request header allows it)

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const sslRedirect = require('heroku-ssl-redirect');
+const compression = require('compression');
 
 const app = express();
+app.use(compression()); // send files using compression (if possible)
 app.set('port', process.env.PORT || 5000);
 app.use(sslRedirect()); // redirect HTTP to HTTPS
 app.use(require('express-naked-redirect')({reverse: true})); // redirect www.seattleflu.org to seattleflu.org
