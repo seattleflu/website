2109c6f10ded48ddca4d0c88f3b8da66d9cfc43d
Jover <joverlee521@gmail.com>
2019-08-28 14:54:10 -0700

    Add sample not received display

--- src/components/ReturnOfResults.js
+++ src/components/ReturnOfResults.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { H1, OuterContainer, ContentContainer } from './utils';
 import BarcodeSearchForm from './ParticipantResults/BarcodeSearchForm';
+import SampleNotReceived from "./ParticipantResults/SampleNotReceived";
 
 export default class ReturnOfResults extends React.Component {
     constructor(props) {
@@ -26,6 +27,9 @@ export default class ReturnOfResults extends React.Component {
                     <H1>Return of Results</H1>
                     { this.state.barcode === '' &&
                         <BarcodeSearchForm submitResult={this.setResult}/>
+                    }
+                    { this.state.status === 'notReceived' &&
+                        <SampleNotReceived/>
                     }
                 </ContentContainer>
             </OuterContainer>
