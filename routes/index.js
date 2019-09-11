49918cc8d79fe67196386cc55897e1f95ad29d70
Brynn Tweeddale <brynn@Morpheus.localdomain>
2019-09-11 13:16:59 -0700

    mobile menu

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Seattle Flustudy', header: 'light' })
+  res.render('index', { title: 'Seattle Flustudy', header: 'dark' })
 })
 
 module.exports = router
