2abfc9134f7485ec9cdfd1d6fb66a6843e06e05a
Brynn Tweeddale <brynn@Morpheus.localdomain>
2019-09-12 11:30:33 -0700

    mobile menu overlay

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Seattle Flu Study', header: 'light' })
+  res.render('index', { title: 'Seattle Flu Study', header: 'dark' })
 })
 
 module.exports = router
