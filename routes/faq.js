dd715130330dbeef91e171b6633fcb5cfb658157
Brandon Friar <btfriar@gmail.com>
2019-09-16 16:04:23 -0700

    add faq dropdown and conenct to contentful

--- routes/faq.js
+++ routes/faq.js
@@ -1,9 +1,25 @@
 var express = require('express')
 var router = express.Router()
+var faq = require('../services/faq')
+
+router.use((req, res, next) => {
+  faq
+    .getFaq()
+    .then(faqData => {
+      console.log('PAGE DATA: ' + JSON.stringify(faqData))
+      req.faqData = faqData.items
+      next()
+    })
+    .catch(console.error)
+})
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('faq', { title: 'Seattle Flu Study FAQ', header: 'light' })
+  res.render('faq', {
+    title: 'Seattle Flu Study FAQ',
+    faqData: req.faqData,
+    header: 'light'
+  })
 })
 
 module.exports = router
