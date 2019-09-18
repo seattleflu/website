bcea181fafc2df9fecd5ac852043a22a582f7300
Brandon Friar <btfriar@gmail.com>
2019-09-17 17:01:35 -0700

    add zipcodes

--- enroll/src/components/steps/Main.jsx
+++ enroll/src/components/steps/Main.jsx
@@ -7,11 +7,44 @@ const Main = props => {
   const [ageValue, setAgeValue] = useState('')
 
   useEffect(() => {})
+  const zipCodes = [
+    '98101',
+    '98102',
+    '98103',
+    '98104',
+    '98105',
+    '98106',
+    '98107',
+    '98108',
+    '98109',
+    '98112',
+    '98115',
+    '98116',
+    '98117',
+    '98118',
+    '98119',
+    '98121',
+    '98122',
+    '98125',
+    '98126',
+    '98133',
+    '98134',
+    '98136',
+    '98144',
+    '98146',
+    '98154',
+    '98164',
+    '98174',
+    '98177',
+    '98178',
+    '98195',
+    '98199'
+  ]
 
   function handleChange (event) {
     event.preventDefault()
     if (question == 0) {
-      if (zipValue == '98107') {
+      if (zipCodes.includes(zipValue)) {
         setQuestion(question + 1)
       } else {
         props.handleNextError(props.bouncePage1)
--- routes/kiosks.js
+++ routes/kiosks.js
@@ -1,9 +1,25 @@
 var express = require('express')
 var router = express.Router()
+var kiosks = require('../services/kiosks')
+
+router.use((req, res, next) => {
+  kiosks
+    .getKiosks()
+    .then(kiosksData => {
+      console.log('PAGE DATA: ' + JSON.stringify(kiosksData))
+      req.kiosksData = kiosksData.items
+      next()
+    })
+    .catch(console.error)
+})
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('kiosks', { title: 'Kiosks', header: 'light' })
+  res.render('kiosks', {
+    title: 'Kiosks',
+    header: 'light',
+    kiosksData: req.kiosksData
+  })
 })
 
 module.exports = router
