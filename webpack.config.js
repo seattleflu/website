2006a3ad69dcad7cccab5099313963b997a6f9a2
Thomas Sibley <tsibley@fredhutch.org>
2019-09-24 17:46:02 -0700

    Unify our React "subapps" into a single build that produces multiple bundles
    This greatly simplifies development at the potential cost of not having
    isolated dependencies because one package.json is shared by everything.
    I think this is worth it.  While having entirely independent apps seemed
    like a good idea initially, in practice it worked poorly and involved a
    lot of extra complexity for ~zero gain.
    
    I'm pretty happy with the net of this diff stat:
    
        4884 insertions(+), 31415 deletions(-)

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+/* This is the Webpack configuration to bundle our React "subapps".
+ *
+ * Babel transpiling configuration is in .babelrc with some directory-specific
+ * additions below embedded in the Webpack config.
+ */
+
+const path = require("path");
+
+module.exports = (env) => {
+  const production = env === "production";
+
+  /* These are all our React "subapps" which each have their own route/[name].js
+   * and EJS template under views/[name].js.  They are transpiled by Babel and
+   * bundled by Webpack and can use standard ES6 features not supported by NodeJS
+   * or some older browsers (polyfills are used).
+   *
+   * Each entrypoint gets it own bundle in dist/[name]-bundle.js.
+   */
+  const entrypoints = {
+    current: ['./current/src/index.js'],
+    enroll: ['./enroll/src/index.js'],
+    results: ['./results/src/index.js'],
+    science: ['./science/src/index.js'],
+  };
+
+  return {
+    mode: production ? "production" : "development",
+    entry: entrypoints,
+    output: {
+      filename: '[name]-bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist',
+    },
+    module: {
+      rules: [
+        /* Source in our React subapps use native ES6 module syntax and get
+         * file-scoped, usage-dependent core-js prefills for ES6 via
+         * @babel/preset-env.
+         */
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          include: Object.keys(entrypoints).map(app => path.resolve(__dirname, app)),
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { modules: false, useBuiltIns: 'usage', corejs: 3 }]
+              ],
+            }
+          }
+        },
+
+        /* Source common to our NodeJS server and our React subapps (e.g.
+         * services/contentful.js) use CommonJS module syntax and can only use
+         * language syntax supported by NodeJS.
+         */
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          include: [
+            path.resolve(__dirname, 'routes'),
+            path.resolve(__dirname, 'services'),
+          ],
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { modules: 'commonjs' }]
+              ]
+            }
+          }
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader'
+            }
+          ]
+        },
+        { test: /\.css$/, use: 'css-loader' },
+        { test: /\.svg$/, use: 'svg-url-loader' },
+      ]
+    },
+    plugins: [
+    ]
+  }
+};
