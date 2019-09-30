498c8e1be2a7a7664361836b277b3d6e48d5a2d6
Thomas Sibley <tsibley@fredhutch.org>
2019-09-30 15:28:49 -0700

    Results: Rectify overall changes from Formative with our placeholder page

--- routes/results.js
+++ routes/results.js
@@ -29,7 +29,7 @@ router.use((req, res, next) => {
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('results', {
-    title: 'Results Page',
+    title: 'Your Results (coming soon!)',
     header: 'light',
     nav: 'true',
     enroll: 'true',
