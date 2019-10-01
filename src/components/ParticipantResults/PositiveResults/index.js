87b62ec9fba8966758e04578730e8de2088580bb
Thomas Sibley <tsibley@fredhutch.org>
2019-10-01 10:08:04 -0700

    Delete all season one files

--- src/components/ParticipantResults/PositiveResults/index.js
+++
@@ -1,4 +0,0 @@
-export {default as Flu} from './Flu';
-export {default as RSV} from './RSV';
-export {default as Coronavirus} from './Coronavirus';
-export {default as Enterovirus} from './Enterovirus';
