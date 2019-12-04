cad41437a55b5344228c2eb31d1ed1f8dc35a809
Brandon Friar <btfriar@gmail.com>
2019-12-04 09:43:48 -0800

    change order of kiosks

--- services/kiosks.js
+++ services/kiosks.js
@@ -3,7 +3,8 @@ var client = require('./contentful').client
 async function getKiosks () {
   try {
     const result = await client.getEntries({
-      content_type: 'kiosks'
+      content_type: 'kiosks',
+      order: 'sys.createdAt',
     })
     return result
   } catch (error) {
