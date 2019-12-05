dff0763b9d869d9f07c9198980dff9b49ab857e5
Thomas Sibley <tsibley@fredhutch.org>
2019-12-04 17:49:18 -0800

    Current: Note a future improvement

--- current/src/components/FluMap/geometry/index.js
+++ current/src/components/FluMap/geometry/index.js
@@ -2,5 +2,7 @@ import { fromJS as immutable } from "immutable";
 import seattleNeighborhoods from "./seattle-neighborhoods.geojson";
 
 export default {
+  // XXX TODO: Use better shapes which are clipped to land and don't cover
+  // water.  This will improve the look of the map.
   seattleNeighborhoods: immutable(seattleNeighborhoods)
 };
