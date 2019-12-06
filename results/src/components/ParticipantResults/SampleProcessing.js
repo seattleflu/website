fdf5488e12b3016d97b737272b88398585e9a4ed
Jover <joverlee521@gmail.com>
2019-12-05 16:11:33 -0800

    return results: update SampleProcessing style
    Edit the question listed to look similar to barcode FAQs on results
    pages.

--- results/src/components/ParticipantResults/SampleProcessing.js
+++ results/src/components/ParticipantResults/SampleProcessing.js
@@ -1,23 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useContext, Fragment } from 'react';
 import * as Markdown from 'react-markdown';
 
 import { resultsContext } from '../ReturnOfResults';
-import { Feature, FeatureH3, UnorderedList, Br } from '../styledComponents';
+import { Feature, FeatureH3, CenteredParagraph } from '../styledComponents';
 
 export default function SampleProcessing() {
   const { content } = useContext(resultsContext);
 
   return(
     <div>
-      <Feature title={<Markdown source={content.title} renderers={{paragraph: FeatureH3}}/>}
+      <Feature
         buttonLink={content.buttonLink}
         buttonText={content.buttonText}>
-        <FeatureH3>
-          {content.paragraphOne}
-        </FeatureH3>
+        <Fragment>
+          <Markdown source={content.title} renderers={{paragraph: FeatureH3}}/>
+          <FeatureH3>{content.paragraphOne}</FeatureH3>
+        </Fragment>
       </Feature>
-      <div>
-        <Markdown source={content.paragraphTwo} renderers={{ list: UnorderedList }}/>
+      <div className="text-center mb-md-5">
+        <Markdown source={content.paragraphTwo} renderers={{ paragraph: CenteredParagraph }}/>
       </div>
     </div>
   )
