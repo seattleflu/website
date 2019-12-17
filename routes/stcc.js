6bbd9bd9a72f91dac4df676bd29820029174b026
Brandon Friar <btfriar@gmail.com>
2019-12-17 15:17:35 -0800

    add markdown to stcc

--- routes/stcc.js
+++ routes/stcc.js
@@ -1,56 +1,62 @@
-var express = require('express')
-var router = express.Router()
+var express = require ('express');
+var router = express.Router ();
 
-var page = require('../services/page')
-var site = require('../services/site')
+var page = require ('../services/page');
+var site = require ('../services/site');
 
-var md = require('markdown-it')({
-  html: true
-})
-var markdownItAttrs = require('markdown-it-attrs')
+var md = require ('markdown-it') ({
+  html: true,
+});
+var markdownItAttrs = require ('markdown-it-attrs');
+md.use (markdownItAttrs, {
+  // optional, these are default options
+  leftDelimiter: '{',
+  rightDelimiter: '}',
+  allowedAttributes: [], // empty array = all attributes are allowed
+});
 
-router.use((req, res, next) => {
+router.use ((req, res, next) => {
   site
-    .getSiteData()
-    .then(siteData => {
-      req.siteData = siteData.items
-      next()
+    .getSiteData ()
+    .then (siteData => {
+      req.siteData = siteData.items;
+      next ();
     })
-    .catch(console.error)
-})
+    .catch (console.error);
+});
 
-router.use((req, res, next) => {
+router.use ((req, res, next) => {
   page
-    .getPageData('stcc')
-    .then(pageData => {
-      req.pageData = pageData.items
-      if(pageData.items[0].fields.showMenu != null){
-        var nav = pageData.items[0].fields.showMenu
-        req.nav = nav.toString();
-      }else{
-        req.nav = 'true'
+    .getPageData ('stcc')
+    .then (pageData => {
+      req.pageData = pageData.items;
+      if (pageData.items[0].fields.showMenu != null) {
+        var nav = pageData.items[0].fields.showMenu;
+        req.nav = nav.toString ();
+      } else {
+        req.nav = 'true';
       }
-      if(pageData.items[0].fields.showJoinTheStudyAfterMenu != null){
-        var enroll = pageData.items[0].fields.showJoinTheStudyAfterMenu
-        req.enroll = enroll.toString();
-      }else{
-        req.enroll = 'true'
+      if (pageData.items[0].fields.showJoinTheStudyAfterMenu != null) {
+        var enroll = pageData.items[0].fields.showJoinTheStudyAfterMenu;
+        req.enroll = enroll.toString ();
+      } else {
+        req.enroll = 'true';
       }
-      next()
+      next ();
     })
-    .catch(console.error)
-})
+    .catch (console.error);
+});
 
-router.get('/', function(req,res,next){
-  var baseUrl = req.get('host')
+router.get ('/', function (req, res, next) {
+  var baseUrl = req.get ('host');
   var pageUrl = req.baseUrl;
-  req.pageUrl =  baseUrl + pageUrl
-  next()
-})
+  req.pageUrl = baseUrl + pageUrl;
+  next ();
+});
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('stcc', {
+router.get ('/', function (req, res, next) {
+  res.render ('stcc', {
     title: 'S&T Complete',
     header: 'light',
     md: md,
@@ -59,8 +65,8 @@ router.get('/', function (req, res, next) {
     logos: 'false',
     pageData: req.pageData,
     siteData: req.siteData,
-    pageUrl:req.pageUrl
-  })
-})
+    pageUrl: req.pageUrl,
+  });
+});
 
-module.exports = router
+module.exports = router;
