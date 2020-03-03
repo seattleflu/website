7d50124a7322cf1a1b5747f5a0248023fdcda168
Brynn Tweeddale <brynn@Morpheus.localdomain>
2020-03-02 16:15:33 -0800

    new menu for mobile

---
+++ public/js/menu.js
@@ -0,0 +1,35 @@
+function menuActive () {
+    var menuWrapper = document.getElementById ('menu-wrap');
+    var menu = document.getElementById ('toggle-button');
+    console.log ('menu clicked');
+    if ($ ('#toggle-button').hasClass ('button-open')) {
+      $ ('.toggle-wrap').fadeOut(250);
+      $ ('.overlay').fadeOut (500);
+      $ ('.toggle-wrap').addClass ('toggle-wrap-relative');
+      $ ('.menu-sidebar li > a.test').removeClass ('test');
+      $ ('.toggle-wrap').fadeIn(250);
+      menuWrapper.classList.remove ('menu-show');
+      menu.classList.remove ('button-open');
+
+    } else {
+      $ ('.toggle-wrap').hide();
+      $ ('.overlay').fadeIn (250);
+      $ ('.menu-sidebar li > a').each (function (index) {
+        $ (this).delay (50 * index).queue (function () {
+          $ (this).toggleClass ('test').dequeue ();
+        });
+        menuWrapper.classList.add ('menu-show');
+        menu.classList.add ('button-open');
+      });
+      $ ('.toggle-wrap').removeClass ('toggle-wrap-relative');
+      $ ('.toggle-wrap').delay(100).fadeIn(250);
+    }
+  }
+  
+  window.onload = function () {
+    var menu = document.getElementById ('toggle-button');
+    if (menu) {
+      menu.addEventListener ('click', menuActive);
+    }
+  };
+  
