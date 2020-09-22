1c377917ae3d0118ae9db2c4c8d0a3cdfa7f0411
Brandon Friar <btfriar@gmail.com>
2020-09-22 16:44:34 -0700

    test route

--- routes/uw.js
+++ routes/uw.js
@@ -49,7 +49,7 @@ router.get('/', function(req,res,next){
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('UW', {
+  res.render('uw', {
     title: 'UW',
     header: 'dark',
     md: md,
