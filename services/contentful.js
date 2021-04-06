c0a992a237cac160d638d0850fce939bc95438e5
Brynn Tweeddale <brynn@Brynns-Work-Computer.local>
2021-04-06 12:45:36 -0700

    apple study live minus faq page

--- services/contentful.js
+++ services/contentful.js
@@ -1,18 +1,18 @@
 var contentful = require('contentful')
 
-// var config = {
-//   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "khzGbzJg6rnFLJNw6wlkEw_jCNzdza1ZEJAOeyK3Z5o",
-//   space: process.env.CONTENTFUL_SPACE || "mw94neez9vid",
-//   host: "cdn.contentful.com",
-//   insecure: false
-// }
+ var config = {
+   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "khzGbzJg6rnFLJNw6wlkEw_jCNzdza1ZEJAOeyK3Z5o",
+   space: process.env.CONTENTFUL_SPACE || "mw94neez9vid",
+   host: "cdn.contentful.com",
+   insecure: false
+ }
 
-var config = {
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "3WjCDnhn0P6Xlm2-V5grdDPpBCRgpGcMfq7IM-7wld8",
-  space: process.env.CONTENTFUL_SPACE || "mw94neez9vid",
-  host: "preview.contentful.com",
-  insecure: false
-}
+//var config = {
+//  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "3WjCDnhn0P6Xlm2-V5grdDPpBCRgpGcMfq7IM-7wld8",
+//  space: process.env.CONTENTFUL_SPACE || "mw94neez9vid",
+//  host: "preview.contentful.com",
+//  insecure: false
+//}
 
 var client = contentful.createClient(config)
 
