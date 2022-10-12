15d81625de6edee466eb7a88d549c8e0d0643225
Brandon Friar <friar@Brandons-MacBook-Pro.local>
2022-10-12 08:51:54 -0700

    test preview

--- services/contentful.js
+++ services/contentful.js
@@ -1,18 +1,18 @@
 var contentful = require('contentful')
 
-//  var config = {
-//    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "khzGbzJg6rnFLJNw6wlkEw_jCNzdza1ZEJAOeyK3Z5o",
-//    space: process.env.CONTENTFUL_SPACE || "mw94neez9vid",
-//    host: "cdn.contentful.com",
-//    insecure: false
-//  }
+ var config = {
+   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "nNdyqY7yYFAht1SeM-V644PMhHAr2-OxoQM3cAkv1xs",
+   space: process.env.CONTENTFUL_SPACE || "mw94neez9vid",
+   host: "https://preview.contentful.com",
+   insecure: false
+ }
 
-var config = {
- accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "OzpJ8zbBPkh0ypL3EIwJSZpIV2BAiHJERFUk4mjzsrc",
- space: process.env.CONTENTFUL_SPACE || "mw94neez9vid",
- host: "cdn.contentful.com",
- insecure: false
-}
+// var config = {
+//  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "OzpJ8zbBPkh0ypL3EIwJSZpIV2BAiHJERFUk4mjzsrc",
+//  space: process.env.CONTENTFUL_SPACE || "mw94neez9vid",
+//  host: "cdn.contentful.com",
+//  insecure: false
+// }
 
 var client = contentful.createClient(config)
 
